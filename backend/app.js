68365a29843ce450d13286bf57699e8357055185
Prathmesh <loharprathmesh123@example.com>
2025-11-16 19:14:19 +0530

    new controller get users

--- backend/app.js
+++ backend/app.js
@@ -3,15 +3,25 @@ const app = express();
 require("dotenv").config();
 require("./connection/conn")
 const userApis = require("./controllers/User");
+const allUsers = require("./controllers/GetUsers");
 const projectApis = require("./controllers/Project");
 const taskApis = require("./controllers/Task");
+const cors = require("cors");
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
 
 app.use(express.json());
 
 app.use("/api/v1/auth",userApis);
 app.use("/api/v1/projects",projectApis);
 app.use("/api/v1/tasks",taskApis);
+app.use("/api/v1/users",allUsers);
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
 });
+
